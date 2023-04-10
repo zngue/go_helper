3065a540cfc8f16215ba7562305c852aa53a06e9
zngue <zngfue@163.com>
2023-04-10 22:34:10 +0800

    fix: 更新文件信息

--- push.sh
+++ push.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-msg=${2:-"Release ${1}"}
-version=${1:-"v1.0.20"}
-git tag -a "${version}" -m "${msg}"
-git push origin "${version}"
-
+msg=${1:-"update"}
 git add .
-
-git commit -m "${msg}"
-
+git commit -m "fix: ${msg}"
 git push
