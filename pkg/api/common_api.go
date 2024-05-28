328bb2c23fe4f7da354981dc982ce522c3c8c164
zngue <zngfue@163.com>
2024-05-28 13:49:57 +0800

    更新api接口数据

--- pkg/api/common_api.go
+++ pkg/api/common_api.go
@@ -12,20 +12,20 @@ type CommonApi interface {
 	List() gin.HandlerFunc
 	ListPage() gin.HandlerFunc
 }
-type Common[T any, C any] struct {
-	common CommonService[T, C]
+type Common[T any] struct {
+	common CommonService[T]
 }
 
-func NewCommon[T any, C any](common CommonService[T, C]) CommonApi {
-	data := new(Common[T, C])
+func NewCommon[T any](common CommonService[T]) CommonApi {
+	data := new(Common[T])
 	data.common = common
 	return data
 }
 
 type RouterFn func(router *gin.RouterGroup)
 
-func Router[T any, C any](routerName string, data CommonService[T, C], api *gin.RouterGroup, fns ...RouterFn) {
-	var apiData = NewCommon[T, C](data)
+func Router[T any](routerName string, data CommonService[T], api *gin.RouterGroup, fns ...RouterFn) {
+	var apiData = NewCommon[T](data)
 	var router = api.Group(routerName)
 	router.GET("content", apiData.Content())
 	router.GET("list", apiData.List())
@@ -43,56 +43,56 @@ func Router[T any, C any](routerName string, data CommonService[T, C], api *gin.
 	return
 }
 
-func (c *Common[T, C]) Content() gin.HandlerFunc {
+func (c *Common[T]) Content() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var content, err = c.common.Content(ctx)
 		DataWithErr(ctx, err, content)
 	}
 }
 
-func (c *Common[T, C]) Status() gin.HandlerFunc {
+func (c *Common[T]) Status() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var err = c.common.Status(ctx)
 		DataWithErr(ctx, err, nil)
 	}
 }
 
-func (c *Common[T, C]) UpdateFiled() gin.HandlerFunc {
+func (c *Common[T]) UpdateFiled() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var err = c.common.UpdateFiled(ctx)
 		DataWithErr(ctx, err, nil)
 	}
 }
 
-func (c *Common[T, C]) Add() gin.HandlerFunc {
+func (c *Common[T]) Add() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var err = c.common.Add(ctx)
 		DataWithErr(ctx, err, nil)
 	}
 }
 
-func (c *Common[T, C]) Update() gin.HandlerFunc {
+func (c *Common[T]) Update() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var err = c.common.Update(ctx)
 		DataWithErr(ctx, err, nil)
 	}
 }
 
-func (c *Common[T, C]) Delete() gin.HandlerFunc {
+func (c *Common[T]) Delete() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var err = c.common.Delete(ctx)
 		DataWithErr(ctx, err, nil)
 	}
 }
 
-func (c *Common[T, C]) List() gin.HandlerFunc {
+func (c *Common[T]) List() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var list, err = c.common.List(ctx)
 		DataWithErr(ctx, err, list)
 	}
 }
 
-func (c *Common[T, C]) ListPage() gin.HandlerFunc {
+func (c *Common[T]) ListPage() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var list, count, err = c.common.ListPage(ctx)
 		DataWithErr(ctx, err, gin.H{
--- pkg/api/common_service.go
+++ pkg/api/common_service.go
@@ -2,13 +2,13 @@ package api
 
 import "github.com/gin-gonic/gin"
 
-type CommonService[T any, C any] interface {
+type CommonService[T any] interface {
 	Add(ctx *gin.Context) (err error)
 	Update(ctx *gin.Context) (err error)
 	Delete(ctx *gin.Context) (err error)
 	List(ctx *gin.Context) (items []*T, err error)
 	ListPage(ctx *gin.Context) (list []*T, count int64, err error)
 	UpdateFiled(ctx *gin.Context) (err error)
-	Content(ctx *gin.Context) (content *C, err error)
+	Content(ctx *gin.Context) (content *T, err error)
 	Status(ctx *gin.Context) (err error)
 }
